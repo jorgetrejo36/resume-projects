ef6ac83561fc0b5ec33e81ca8e454154b5f7a358
jorgetrejo36 <65737813+jorgetrejo36@users.noreply.github.com>
2022-08-21 13:05:59 -0400

    Blog Web Application

---
+++ BlogWebApp/app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const ejs = require("ejs");
+const _ = require('lodash');
+const port = 3000;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+require('dotenv').config();
+
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const homeStartingContent = "This is a simple blog website where you can create blogs which are all saved locally. To add a new blog go to this URL: \"localhost:3000/compose\". To delete all blogs go to: \"localhost:3000/reset\".";
+const aboutContent = "This is an example about page. Any information pertaining to the origin of this blog website would be put here.";
+const contactContent = "This is an example contact page which would have information such as phone, email, etc.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+// Schema
+
+const postSchema = new Schema({
+  title: String,
+  content: String
+});
+
+// Model
+
+const Post = mongoose.model('Post', postSchema);
+
+app.get('/', (req, res) => {
+
+  Post.find({}, (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', {
+        homeStartingContent: homeStartingContent,
+        posts: posts,
+        _: _
+      });
+    }
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {aboutContent: aboutContent});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {contactContent: contactContent});
+});
+
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+// get route to get individual post when user wants to read more
+app.get('/posts/:postID', (req, res) => {
+
+  const postID = req.params.postID;
+
+  Post.findById(postID, (err, post) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('post', {post: post});
+    }
+  });
+});
+
+// reset the database and delete all blogs
+app.get('/reset', (req, res) => {
+
+  Post.deleteMany({}, (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect('/')
+    }
+  })
+})
+
+app.post('/compose', async (req, res) => {
+  const postTitle = req.body.postTitle;
+  const postBody = req.body.postBody;
+  
+  const post = new Post({
+    title: postTitle,
+    content: postBody
+  });
+
+  await post.save();
+
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log("Server started on port 3000");
+});
